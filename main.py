b8261d6225f821c5ce2b6932df3ed669271ad8d6
Ximing Hu <hu.ximing@hotmail.com>
2022-12-13 21:30:04 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -1,82 +1,68 @@
-import shutil
-from tinytag import TinyTag  # https://pypi.org/project/tinytag/
+from tinytag import TinyTag
 import os
-import glob
 import sys
 
-MIN_ALBUM_SONGS = 3
-FILE_EXTENTION = ['mp3', 'flac']
+MIN_ALBUM_FILES = 3
+AUDIO_EXTENTIONS = ('mp3', 'flac')
 
 
-class Album:
-
-    def __init__(self) -> None:
-        self.name: str = None
-        self.songs: list[str] = []
-        self.dir: str = None
-
-    def __repr__(self) -> str:
-        s = self.name + '\n'
-        for song in self.songs:
-            s += '\t' + song + '\n'
-        return s
-
-    # update dir to a valid directory name
-    def get_dir_name(self) -> None:
-        dir = self.name
-        dir = dir.replace(':', '_')
-        for c in '\\/*?"<>|':
-            dir = dir.replace(c, '')
-        dir = dir.strip()
-        self.dir = dir
+# convert string to a valid directory name
+def convert_to_valid_dirname(s: str):
+    for c in ':\\/*?"<>|':
+        s = s.replace(c, '_')
+    s.strip()
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        os.chdir(sys.argv[1])
+        music_dir = sys.argv[1]
     else:
-        os.chdir(input('path of music directory: '))
+        print('ERROR: Please specify the directory in the argument.')
+        exit()
 
-    file_list: list[str] = []
-    for ext in FILE_EXTENTION:
-        file_list += glob.glob('*.' + ext)
-
-    album_map: dict[str, Album] = {}
-
-    # update map/dict
-    for f in file_list:
-        tag = TinyTag.get(f)
-        name = tag.album
-
-        if name in album_map:
-            album_map[name].songs.append(f)
-        else:
-            album = Album()
-            album.name = name
-            album.songs.append(f)
-            album.get_dir_name()
-            album_map[name] = album
-
-    # create directories and move files
+    album_files: dict[str, list[str]] = {}
     count_mv = 0
     count_mkdir = 0
 
-    for name in album_map:
-        album = album_map[name]
-        print(album)
-        if len(album.songs) < MIN_ALBUM_SONGS:
+    # Iterate over the files in the directory
+    for file in os.listdir(music_dir):
+        if not file.endswith(AUDIO_EXTENTIONS):
             continue
 
-        # move songs to the same directory
-        # if they have the same formatted album path
-        if not os.path.exists(album.dir):
-            os.mkdir(album.dir)
+        # Read the metadata from the file
+        metadata = TinyTag.get(os.path.join(music_dir, file))
+
+        # Get the album from metadata
+        album = metadata.album
+
+        # Keep track of which files are in each album
+        if album not in album_files:
+            album_files[album] = []
+        album_files[album].append(file)
+
+    for album, files in album_files.items():
+        if len(files) < MIN_ALBUM_FILES:
+            print('Skipping album "%s",' % album)
+            print('because it has less than %d files. count = %d' %
+                  (MIN_ALBUM_FILES, len(files)))
+            print()
+            continue
+
+        # Create the directory for the album, if it doesn't already exist
+        album_dir = os.path.join(music_dir, convert_to_valid_dirname(album))
+        if not os.path.exists(album_dir):
+            print('Creating directory: "%s"' % album_dir)
+            os.makedirs(album_dir)
             count_mkdir += 1
-        for f in album.songs:
-            shutil.move(f, album.dir)
+
+        # Move the file to the album directory
+        for file in files:
+            print('Moving file "%s" to directory "%s"' % (file, album_dir))
+            os.rename(os.path.join(music_dir, file),
+                      os.path.join(album_dir, file))
             count_mv += 1
 
     # summarize
     print('===== Summarize =====')
-    print('Number of created directories: ', count_mkdir)
-    print('Number of files moved: ', count_mv)
+    print('Number of created directories: %d' % count_mkdir)
+    print('Number of files moved: %d' % count_mv)
